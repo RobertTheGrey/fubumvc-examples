03fc92777f25672d03960bfa7073a3d0a22afc15
joshuaflanagan <joshuaflanagan@gmail.com>
2010-02-02 19:04:58 -0600

    Create project to demonstrate varying output based on the Accept header

---
+++ src/Outputs/VaryByAcceptHeader/SimpleWebsite/SimpleWebsiteFubuRegistry.cs
@@ -0,0 +1,28 @@
+using FubuMVC.Core;
+using SimpleWebsite.Controllers;
+using SimpleWebsite.Core;
+
+namespace SimpleWebsite
+{
+    public class SimpleWebsiteFubuRegistry : FubuRegistry
+    {
+        public SimpleWebsiteFubuRegistry()
+        {
+            IncludeDiagnostics(true);
+
+            Applies.ToThisAssembly();
+
+            Actions.IncludeTypesNamed(x => x.EndsWith("Controller"));
+
+            Routes.IgnoreControllerNamespaceEntirely();
+
+            Output.ToJson.WhenCallMatches(action => action.Returns<AjaxResponse>());
+
+            Views.TryToAttach(findViews => findViews.by_ViewModel_and_Namespace_and_MethodName());
+
+            // Note: Outside of a sample application, you would only configure services that Fubu requires within your FubuRegistry
+            // Non-Fubu services should be configured through your container in the usual way (StructureMap Registry, etc)
+            Services(s => s.AddService<IRepository, FakeRepository>());
+        }
+    }
+}
