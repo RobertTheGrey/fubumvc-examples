5d6e4c423d7b5490b835796957db46e48a275175
Robert Greyling <robertgreyling@gmail.com>
2010-02-12 01:34:26 +0000

    Fixed references and cleanup

--- src/ViewEngines/Spark/MasterViewsAndPartials/Models/Question/QuestionListViewModel.cs
+++ src/ViewEngines/Spark/MasterViewsAndPartials/Models/Question/QuestionListViewModel.cs
@@ -5,9 +5,9 @@ namespace MasterAndPartialViews.Models.Question
     public class QuestionListViewModel
     {
         public List<string> Questions = new List<string>
-                                            {
-                                                "What is the answer to life, the universe, and everything?",
-                                                "What is the first decimal place of Pi?"
-                                            };
+        {
+            "What is the answer to life, the universe, and everything?",
+            "What is the first decimal place of Pi?"
+        };
     }
 }
